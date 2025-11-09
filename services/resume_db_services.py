4834ef1c69fa64f9402029fdf0875e2a604f31c8
Govind <gcmeena51@gmail.com>
2025-11-09 22:53:28 +0530

    get all resumes

--- services/resume_db_services.py
+++ services/resume_db_services.py
@@ -1,1 +1,16 @@
 import asyncpg
+
+
+async def create_connection():
+    conn = await asyncpg.connect(user='postgres', password='password',
+                                 database='resume_screener', host='localhost', port=5434)
+    return conn
+
+
+async def get_all_resumes():
+    conn = await create_connection()
+    
+    rows = await conn.fetch("SELECT * FROM resumes ORDER BY created_at DESC;")
+    await conn.close()
+    
+    return [dict(row) for row in rows] 
