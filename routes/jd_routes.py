901adc7050c3192218e5dc434a39d4c4bc4ba46c
Govind <gcmeena51@gmail.com>
2025-11-10 22:33:41 +0530

    get endpoint for jd

--- routes/jd_routes.py
+++ routes/jd_routes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from services.jd_services import insert_jd_into_db 
+from services.jd_services import insert_jd_into_db, get_all_jds
 from schemas.jd_schema import JobDescription
 
 
@@ -13,6 +13,15 @@ async def upload_jd(jd: JobDescription):
 
     response = await insert_jd_into_db(jd.jd_text, jd.title, jd.company_name)
     return response
+
+@router.get("/jds")
+async def fetch_all_jds():
+    """
+    Fetch all Job Descriptions from the database.
+    """
+
+    jds = await get_all_jds()
+    return jds
 
 
  
--- services/jd_services.py
+++ services/jd_services.py
@@ -1,5 +1,6 @@
 import uuid
 from services.db_services import create_connection
+from utils.utility import format_datetime_to_ist
 
 
 async def insert_jd_into_db(jd_text, title, company_name):
@@ -19,3 +20,24 @@ async def insert_jd_into_db(jd_text, title, company_name):
     await conn.close()
     
     return {"jd_id": str(jd_id), "message": "Job description inserted successfully."}
+
+
+async def get_all_jds():
+    """
+        Retrieve all job descriptions from the database.
+    """
+
+    conn = await create_connection()
+    
+    rows = await conn.fetch("SELECT * FROM job_descriptions ORDER BY created_at DESC;")
+    await conn.close()
+    response = []
+    for row in rows:
+        response.append({
+            "jd_id": row["jd_id"],
+            "title": row["title"],
+            "company_name": row["company_name"],
+            "jd_text": row["jd_text"],
+            "created_at": format_datetime_to_ist(row["created_at"])
+        })      
+    return response
